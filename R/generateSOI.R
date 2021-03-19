c94befc6652130f68e96ec9c271275de8c26894b
RogerGinBer <alrgibe9@gmail.com>
2021-03-19 18:49:47 +0100

    detectSOIpeaks is functional
    The function now works just as detectChromPeaks: MSnExp goes in, XCMSnExp goes out with the SOI lists for each file. #2
    I've split the SOI information into ChromPeaks matrix and chromPeakData for the rest of SOI information #3
    I've tested that the function works in the vignette, feel free to try it out @jorainer. Just make sure to update RHermes first, since I've made some critical adjustments there too

--- R/generateSOI.R
+++ R/generateSOI.R
@@ -39,13 +39,38 @@ findSOIpeaks <- function(MSnExp, DBfile, adfile){
     struct@metadata@filenames <- c(struct@metadata@filenames, files)
     
     struct <- findSOI(struct, getSOIpar(), fileID = seq_along(files))
-    
     SOIs <- do.call("rbind", lapply(seq_along(files), function(soi){
         soi_list <- struct@data@SOI[[soi]]@SOIList
-        soi_list$file <- files[[soi]]
+        
+        mzs <- lapply(soi_list$peaks, calculateMz)
+        soi_list$mzmin <- sapply(mzs, function(x){x[[1]]})
+        soi_list$mzmax <- sapply(mzs, function(x){x[[2]]})
+        soi_list$mz <- sapply(mzs, function(x){x[[3]]})
+        
+        rts <- lapply(soi_list$peaks, calculateRt)
+        soi_list$rtmin <- sapply(rts, function(x){x[[1]]})
+        soi_list$rtmax <- sapply(rts, function(x){x[[2]]})
+        soi_list$rt <- sapply(rts, function(x){x[[3]]})
+        
+        soi_list$into <- lapply(soi_list$peaks, function(x){sum(x[,2])})
+        soi_list$intb <- soi_list$into
+        soi_list$maxo <- soi_list$MaxInt
+
+        soi_list$sample <- soi
         return(soi_list)
-    }))
-    return(SOIs)
+    })) %>% as.data.frame()
+    
+    MSnExp <- as(MSnExp, "XCMSnExp")
+    if (nrow(SOIs) > 0) {
+        names <- c("mz", "mzmin", "mzmax", "rt", "rtmin", "rtmax", "into",
+                    "intb", "maxo", "sample")
+        chromPeaks(MSnExp) <- SOIs[, names] %>% as.matrix() %>%
+            apply(., 2, as.numeric)
+        chromPeakData(MSnExp) <- S4Vectors::DataFrame(SOIs[, 1:10],
+                                                      ms_level = as.integer(1),
+                                                      is_filled = FALSE)
+    }
+    return(MSnExp)
 }
 
 #'@importFrom MSnbase extractSpectraData
@@ -64,4 +89,19 @@ extractToHermes <- function(MSnExp){
     
     #To match with the expected format of RHermes: list(raw, header, filtered)
     return(list(pks, h, pks))
+}
+
+
+calculateMz <- function(peaks){
+    mzmin <- peaks$mz[which.min(peaks$rt)]
+    mzmax <- peaks$mz[which.max(peaks$rt)]
+    mz <- peaks$mz[which.max(peaks$rtiv)]
+    return(list(mzmin, mzmax, mz))
+}
+
+calculateRt <- function(peaks){
+    rtmin <- min(peaks$rt)
+    rtmax <- max(peaks$rt)
+    rt <- peaks$rt[which.max(peaks$rtiv)]
+    return(list(rtmin, rtmax, rt))
 }
