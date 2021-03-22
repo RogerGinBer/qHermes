7029d9bbdf1b98eb61106ac7fe8a66228d82be62
RogerGinBer <alrgibe9@gmail.com>
2021-03-22 19:43:53 +0100

    annotateROI
    Given an XCMSnExp object with calculated peaks and a path to a formula and adduct DB, calculates annotations for each Chrompeak within a ppm range.
    Saves the result in ChromPeakData and returns the updated object #4
    I reuse the internal binarySearch function from RHermes to perform very fast mz matching within the ppm range

---
+++ R/annotateROI.R
@@ -0,0 +1,33 @@
+annotateROI <- function(object, DBfile = NA, adfile = NA, ppm = 3, BPPARAM = bpparam()){
+    struct <- RHermesExp()
+    struct <- setCluster(struct, BPPARAM)
+    ppm <- struct@metadata@ExpParam@ppm
+
+    #Selecting formulas and adducts
+    if(!is.na(DBfile)){
+        if(!is.na(adfile)){
+            struct <- setDB(struct, db = "custom", filename = DBfile,
+                            adductfile = adfile)
+        } else {
+            struct <- setDB(struct, db = "custom", filename = DBfile)
+        }
+    } else {
+        struct <- setDB(struct)
+    }
+    
+    prepro <- RHermes:::preprocessing(struct)
+    IF_DB <- prepro[[1]]
+    IC <- prepro[[2]]
+    
+    pks <- chromPeaks(object) %>% as.data.frame
+    IF_DB[[1]] <- IF_DB[[1]][order(IF_DB[[1]]$m),]
+    anot <- lapply(pks$mz, function(mz){
+        match <- RHermes:::binarySearch(matrix(ncol=1, IF_DB[[1]]$m), mz, ppm)
+        paste(IF_DB[[1]]$f[match], collapse = "_")
+    })
+    cpdata <- chromPeakData(object)
+    cpdata$anot <- anot
+    chromPeakData(object) <- cpdata
+    
+    return(object)
+}
