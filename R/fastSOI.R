b45bd232d179d0c740e082314272a0a842639be0
RogerGinBer <alrgibe9@gmail.com>
2021-05-20 12:27:27 +0200

    Update fastSOI.R

--- R/fastSOI.R
+++ R/fastSOI.R
@@ -156,12 +156,27 @@ plotFeature <- function(XCMSnExp, feature, soi = NULL){
                     data.frame(rt = max(pk$rt), rtiv = 0, samp = pknames[x]))
     })
     plot_data <- do.call(rbind, plot_data)
-    ggplotly(ggplot(plot_data) +
-               geom_polygon(aes(x=rt+mint, y=rtiv, fill = samp),
-                                     alpha = 0.5) +
-               xlab("Retention time (s)") + ylab("Intensity") +
-               ggtitle(pks$formula[1])
-               )
+    
+    p1 <- ggplotly(ggplot(plot_data) +
+      geom_polygon(aes(x=rt+mint, y=rtiv, fill = samp),
+                   alpha = 0.5) +
+      xlab("Retention time (s)") + ylab("Intensity") +
+      ggtitle(pks$formula[1]))
+    
+    integral <- plot_data %>% group_by(samp) %>%
+      summarise(intensity = sum(rtiv))
+    
+    p2 <- ggplotly(ggplot(integral) +
+                     geom_point(aes(x=intensity, y=0, color = samp),
+                                show.legend = FALSE)+
+                     scale_x_log10() +
+                     theme(axis.text.y = element_blank(),
+                           axis.ticks.y = element_blank())) %>% hide_legend()
+    for (i in seq_len(nrow(integral))){
+      p2$x$data[[i]]$text <- c(p2$x$data[[i]]$text, "") 
+      p2$x$data[[i]]$showlegend <- FALSE
+    }
+    subplot(p1, p2, nrows = 2, heights = c(0.9, 0.1), which_layout = 1)
 } 
 
 #'@export
